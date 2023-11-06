4d3a9951cf2475cb941794f2ad31fef2d47d913f
MD-Suhad <ssuhad490@gmail.com>
2023-11-06 22:11:49 +0600

    project featured added

--- src/components/Pages/ImageGallery/ImgGallery.jsx
+++ src/components/Pages/ImageGallery/ImgGallery.jsx
@@ -1,9 +1,63 @@
 import React from 'react';
 import { Container, Row,Col,Button } from 'react-bootstrap';
 import './ImgGallery.css';
+import {Gallery} from 'require("../images/image-1.png")'; 
+
+
 
 
 const ImgGallery = () => {
+  
+   const imagesGallery = [
+
+     
+
+
+
+
+
+
+      {
+         id:1,
+         img:require('./images/images-1.png')
+      },
+      {
+         id:2,
+         img:require('./images/images-1.png')
+      }
+      // {
+      //    id:3,
+      //    img:'./images/image-3.webp'
+      // },
+      // {
+      //    id:4,
+      //    img:'./images/image-4.webp'
+      // },
+      // {
+      //    id:5,
+      //    img:'./images/image-5.webp'
+      // },
+      // {
+      //    id:6,
+      //    img:'./images/image-6.webp'
+      // },
+      // {
+      //    id:7,
+      //    img:'./images/image-7.webp'
+      // },
+      // {
+      //    id:8,
+      //    img:'./images/image-8.webp'
+      // },
+      // {
+      //    id:9,
+      //    img:'./images/image-9.webp'
+      // },
+      // {
+      //    id:10,
+      //    img:'./images/image-10.webp'
+      // }
+   ];
 
    return (
       <Container>
@@ -20,7 +74,19 @@ const ImgGallery = () => {
             </Row>
             <Row>
                <Col xs={12} md={12}>
-                  
+                 <div className='image-gallery'>
+                   {
+                     imagesGallery.map((image) => {
+                        <li key ={ image.id}>
+                           <img src ={require(image.img)} alt="Not  supported" />
+                        </li>
+                        
+                     })
+                   }
+                 </div>
+                 <div>
+
+                 </div>
                </Col>
             </Row>
          </Row>
