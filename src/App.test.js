fed6f567657b149a0a16ce6fe7e9ffa82d008bd9
MD-Suhad <ssuhad490@gmail.com>
2023-11-11 09:41:40 +0600

    apptest function error

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,13 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
+// import { render, screen } from '@testing-library/react';
+// import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+// test('renders learn react link', () => {
+//   render(<App />);
+//   const linkElement = screen.getByText(/learn react/i);
+//   expect(linkElement).toBeInTheDocument();
+// });
+// function greaterThan(n) {
+//   return function(m) { return m > n; };
+// }
+// let greaterThan10 = greaterThan(10);
+// console.log(greaterThan10(11))
